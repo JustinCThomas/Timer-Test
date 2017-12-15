dfaae4ab78665e91fd083ae6111ec7660dad11de
Justin Thomas <ThomasCJustin@gmail.com>
2017-12-15 10:35:56 -0500

    Add Milliseconds

--- javascript/timer.js
+++ javascript/timer.js
@@ -2,12 +2,13 @@ const timer = document.getElementById("timer");
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
+const milliseconds = document.getElementById("milliseconds");
 const pauseBtn = document.getElementById("pausebtn");
 const resumeBtn = document.getElementById("resumebtn");
 
 // Add Milliseconds for easier debugging
 
-let initialCount = (4 * 60 * 60); // hours times minutes times seconds
+let initialCount = (4 * 60 * 60 * 1000); // hours times minutes times seconds times milliseconds
 let date = new Date().getMilliseconds();
 let difference;
 let timerInterval;
@@ -15,21 +16,23 @@ let timerInterval;
 let activeButton = "pause";
 
 function init() {
-  hours.innerHTML = initialCount / (60 * 60) % 24;
-  minutes.innerHTML = initialCount / (60) % 60;
-  seconds.innerHTML = initialCount % 60;
-  timerInterval = setInterval(updateTimer , 1000);
+  hours.innerHTML = initialCount / (60 * 60 * 1000) % 24;
+  minutes.innerHTML = initialCount / (60 * 1000) % 60;
+  seconds.innerHTML = (initialCount * 1000) % 60;
+  milliseconds.innerHTML = Math.floor((initialCount) % 1000);
+  timerInterval = setInterval(updateTimer , 10);
 
   pauseBtn.addEventListener('click', pauseTimer);
   resumeBtn.addEventListener('click', resumeTimer);
 }
 
 let updateTimer = () => {
-  initialCount--;
+  initialCount -= 10;
 
-  hours.innerHTML = Math.floor(initialCount / (60 * 60) % 24);
-  minutes.innerHTML = Math.floor((initialCount / 60) % 60);
-  seconds.innerHTML = Math.floor(initialCount % 60);
+  hours.innerHTML = Math.floor(initialCount / (60 * 60 * 1000) % 24);
+  minutes.innerHTML = Math.floor((initialCount / (60 * 1000)) % 60);
+  seconds.innerHTML = Math.floor((initialCount / 1000) % 60);
+  milliseconds.innerHTML = Math.floor((initialCount) % 1000);
   date = new Date().getMilliseconds();
 }
 
@@ -45,7 +48,7 @@ let pauseTimer = () => {
 let resumeTimer = () => {
   if (activeButton === "resume") {
     setTimeout(updateTimer, difference);
-    timerInterval = setInterval(updateTimer , 1000);
+    timerInterval = setInterval(updateTimer , 10);
     activeButton = "pause";
   }
 }
